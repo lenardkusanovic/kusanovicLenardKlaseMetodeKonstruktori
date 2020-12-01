6e5b309f36770f3b8349aea1cc715afa960175f8
lenardkusanovic <55271599+lenardkusanovic@users.noreply.github.com>
2020-12-01 22:12:34 +0100

    Add files via upload

---
+++ kusanovicLenardKlaseMetodeKonstruktori/kusanovicLenardKlaseMetodeKonstruktori/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kusanovicLenardKlaseMetodeKonstruktori
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Proceduralni x = new Proceduralni();
+            Objektni y = new Objektni();
+            Funkcionalni z = new Funkcionalni();
+
+            Console.WriteLine("Upišite postotak za Proceduralni tip jezika: ");
+            x.Postotak = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Upišite postotak za Objektni tip jezika: ");
+            y.Postotak = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Upišite postotak za Funkcionalni tip jezika: ");
+            z.Postotak = Convert.ToDouble(Console.ReadLine());
+
+
+            if (x.Postotak + y.Postotak + z.Postotak != 100)
+            {
+                Console.WriteLine("Neispravni podatci! ");
+            }
+
+            else
+            {
+                if (x.Postotak > y.Postotak && x.Postotak > z.Postotak)
+                {
+                    Console.WriteLine("Najzatupljeniji tip jezika je Proceduralni ");
+                }
+                else if (y.Postotak > z.Postotak)
+                {
+                    Console.WriteLine("Najzatupljeniji tip jezika je Objektni ");
+                }
+                else
+                {
+                    Console.WriteLine("Najzatupljeniji tip jezika je Funkcionalni ");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
